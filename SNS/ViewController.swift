c4835b719e3e9e2da75317502381359ed7f05131
rio.nagasaki <naga_ri@icloud.com>
2022-05-08 11:47:40 +0900

    Initial Commit

---
+++ SNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SNS
+//
+//  Created by Rio Nagasaki on 2022/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
